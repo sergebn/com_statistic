fb532c181a8d112d73281e93baa94bff059df591
sergebn <sergebn@mail.ru>
2015-11-12 14:12:58 +0200

    Begin

---
+++ admin/sql/install.mysql.utf8.sql
@@ -0,0 +1,11 @@
+DROP TABLE IF EXISTS `#__statistic`;
+ 
+CREATE TABLE `#__statistic` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `greeting` varchar(25) NOT NULL,
+   PRIMARY KEY  (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=utf8;
+ 
+INSERT INTO `#__statistic` (`greeting`) VALUES
+        ('This is Statistic!'),
+        ('Good bye Statistic!');
